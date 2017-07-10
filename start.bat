3241293514c001617becdaea89d0fa16f611d5df
Jack Zhuang <zhuangjianguo@me.com>
2017-07-10 16:05:06 +0800

    Meteor 升级到 1.5 版本

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
-set DB_SERVER=192.168.0.23
+set DB_SERVER=192.168.0.21
 set MONGO_URL=mongodb://%DB_SERVER%/steedos
 set MONGO_OPLOG_URL=mongodb://%DB_SERVER%/local
-set MULTIPLE_INSTANCES_COLLECTION_NAME=workflow_instances
-set ROOT_URL=http://127.0.0.1:3000/
+set MULTIPLE_INSTANCES_COLLECTION_NAME=calendar_instances
+set ROOT_URL=http://127.0.0.1:3000/calendar
 meteor run
