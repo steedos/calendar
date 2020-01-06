258f39f7e1fee4b7571bc0f2e962ae1966aa9602
sunhaolin <sunhaolin@hotoa.com>
2020-01-06 11:39:22 +0800

    使用universe:i18n@1.13.0
    steedos:calendar-i18n@0.0.17

--- packages/steedos-calendar-i18n/package.js
+++ packages/steedos-calendar-i18n/package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'steedos:calendar-i18n',
-	version: '0.0.16',
+	version: '0.0.17',
 	summary: 'Steedos calendar system',
 	git: '',
 	documentation: null
@@ -8,7 +8,7 @@ Package.describe({
 
 Package.onUse(function(api) {
 
-	api.use('tap:i18n@1.7.0');
+	api.use('universe:i18n@1.13.0');
 	
 	tapi18nFiles = ['i18n/en.i18n.json', 'i18n/zh-CN.i18n.json']
     api.addFiles(tapi18nFiles, ['client', 'server']);
